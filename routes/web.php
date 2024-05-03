d804ca110c6c204f56f3453784cd496f40885602
Juan Cardenas <juan.cardenas1@upt.edu.co>
2024-05-02 21:44:33 -0500

    Feature: Added home page with the redirections

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/productores', function () {
+    return view('productores');
 });
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
